a0614e8467330b48b46927c74bde355e9e35349a
Mohamed Saqer <mohamed.saqer@outlook.com>
2019-11-29 09:56:37 +0200

    bin model

---
+++ app/Bin.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Bin extends Model
+{
+    protected $table = 'bins';
+    protected $fillable = ['lat','long','status','address'];
+}
---
+++ database/migrations/2019_11_29_075117_create_bins_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBinsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bins', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('lat');
+            $table->string('long');
+            $table->string('address');
+            $table->integer('status')->default(0)->comment('0: empty,1: half,2: full');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bins');
+    }
+}
